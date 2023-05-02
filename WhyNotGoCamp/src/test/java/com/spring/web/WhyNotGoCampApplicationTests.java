c26a4c24a65e4639482001541d119cdf7c1eaa76
jihwan park <jihoan2@naver.com>
2023-05-02 16:55:39 +0900

    jihwan_commit_230502

--- WhyNotGoCamp/src/test/java/com/spring/web/WhyNotGoCampApplicationTests.java
+++ WhyNotGoCamp/src/test/java/com/spring/web/WhyNotGoCampApplicationTests.java
@@ -1,13 +1,21 @@
 package com.spring.web;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class WhyNotGoCampApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    void contextLoads() {
+        // Spring 애플리케이션이 제대로 로딩되는지 테스트
+        assertNotNull(context);
+    }
 }
