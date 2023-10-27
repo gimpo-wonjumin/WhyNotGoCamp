66f62c5356be5c2f959fcb8d057aa7db5faaa566
gimpo-wonjumin <121603106+gimpo-wonjumin@users.noreply.github.com>
2023-10-27 22:27:34 +0900

    Update deploy.sh

--- WhyNotGoCamp/scripts/deploy.sh
+++ WhyNotGoCamp/scripts/deploy.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
 
+REPOSITORY=/home/ec2-user
+PROJECT=wngc
+
+cd $REPOSITORY
+
+JAR_NAME=$(ls $REPOSITORY/$PROJECT/  | grep '.war' | tail -n 1)
+JAR_PATH=$REPOSITORY/$PROJECT/$JAR_NAME
+CURRENT_PID=$(pgrep -fl $JAR_PATH | awk '{print $1}')
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> Nothing to end."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH deploy"
+
+nohup java -jar \
+  $JAR_PATH >> /$REPOSITORY/$PROJECT/log/nohup_log.out 2>&1 &
